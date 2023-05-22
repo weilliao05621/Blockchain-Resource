5437ff6a1e53c9aa98d3c01dd541b317e8794cb6
Weil <f0955710119@gmail.com>
2023-05-22 19:06:11 +0800

    [refactor] update calculation and comments

--- section3/SimpleSwap/contracts/SimpleSwap.sol
+++ section3/SimpleSwap/contracts/SimpleSwap.sol
@@ -36,26 +36,32 @@ contract SimpleSwap is ISimpleSwap, ERC20("Uniswap V2", "UNI-V2") {
     receive() external payable {}
 
     /*
-        1. 拿走別人的代幣，給它 LP 代幣
-        2. 因為已經直接說明 amountA 跟 amountB，不用再判斷 token0 & token1
-        3. 想很久要在哪邊去做實際的 amountA 跟 amountB 判斷
-    */ 
+        1. 拿走別人的代幣，給提供流動性的 address 該 Pair contract 的 ERC20，也就是 LP token
+        2. 因為已經直接說明 amountA 跟 amountB，不用再判斷 token0 & token1 (原本想很久要在哪邊去執行判斷)
+    */
     function addLiquidity(
         uint256 amountAIn,
         uint256 amountBIn
     ) external returns (uint256 amountA, uint256 amountB, uint256 liquidity) {
         require(amountAIn > 0 && amountBIn > 0, "SimpleSwap: INSUFFICIENT_INPUT_AMOUNT");
-        (uint256 _reserve0, uint256 _reserve1) = getReserves(); // gas savings
-        address token0 = _token0; // gas savings
-        address token1 = _token1; // gas savings
+        // 讀取 tokens 和 reserves 來做計算
         /*
-            計算真正放進池子的數量
-                > 這邊忘記要先確定 _reserve 是否為 0
-        */ 
-        uint256 amountAOptimal = _reserve0 == 0 ? amountAIn : amountBIn * _reserve0 / _reserve1;
-        uint256 amountBOptimal = _reserve1 == 0 ? amountBIn : amountAIn * _reserve1 / _reserve0;
+            因為是簡單版本，會直接拿到實際的 input amount。
+            不用再額外去透過 balance0.sub(_reserve0) 去計算這次的交易 amount
+        */
+        (uint256 _reserve0, uint256 _reserve1) = getReserves();
+        (address token0, address token1) = getTokens();
+
+        // quote: 計算真正放進池子的數量
+        (uint256 amountAOptimal, uint256 amountBOptimal) = _quote(amountAIn, amountBIn, _reserve0, _reserve1);
 
         // 表示 amountBIn 算出來的比例多於 amountAIn 提供
+        /*
+            例如原本的池子是 token0:token1 = 1:100, k 為 10 * 1,000 = 10,000。
+            Alice 想增加流動性，但他拿 1 個 token0 和 15 個 token1，公式變成 addLiquidity(1,15);
+            這時候，optimal 會去計算出兩種 token 之間的數量各自去計算時，使用哪一邊的 token 當比例才能完成增加流動性。
+            所以按照當前的 k，顯然是會算出 amountAOptimal = 1 和 amountBOptimal = 10，所以 Alice 剩餘的 5 token1 不會被動用
+        */
         if (amountAIn < amountAOptimal) {
             amountA = amountAIn;
             amountB = amountBOptimal;
@@ -65,57 +71,65 @@ contract SimpleSwap is ISimpleSwap, ERC20("Uniswap V2", "UNI-V2") {
             amountB = amountBIn;
         }
 
-         /*
-            確認有轉進兩種 token 到池子（原本會在 Router 那邊完成）> 寫進 testing
-                > 這邊犯了太早轉錢的錯誤，要算完真正放入的 amount 再轉錢
-        */ 
+        // 確認有轉進兩種 token 到池子（原本會在 Router 那邊完成）> 這邊犯了太早轉錢的錯誤，忘記要算完 optimal 才是真正放入的 amount 這時後才再轉錢
         _safeTransferFrom(token0, amountA);
         _safeTransferFrom(token1, amountB);
-        // 算出池子新的 balance
-        uint256 balance0 = _getTokenBalance(token0);
-        uint256 balance1 = _getTokenBalance(token1);
 
-        // 計算新增的流動性比例：因為沒有最低比例的問題，都直接開平方根
-        liquidity = Math.sqrt(amountA.mul(amountB));
+        // 計算新增的流動性比例
+        uint256 _totalSupply = totalSupply();
+            // 使用 if/else 讓未來更好去擴充不同情況的 liquidity 計算
+        if (_totalSupply == 0) {
+            liquidity = Math.sqrt(amountA.mul(amountB));
+        } else {
+            liquidity = Math.min(amountA.mul(_totalSupply) / _reserve0, amountB.mul(_totalSupply) / _reserve1);
+        }
 
         // 進行 mint 的動作
         require(liquidity > 0, "SimpleSwap: INSUFFICIENT_LIQUIDITY_MINTED");
         _mint(msg.sender, liquidity);
-        // 更新池子
+
+        // 算出池子新的 balance，準備更新池子的 reserves
+        uint256 balance0 = _getTokenBalance(token0);
+        uint256 balance1 = _getTokenBalance(token1);
         _update(balance0, balance1);
-        kLast = balance0 * balance1;
+        kLast = balance0.mul(balance1);
         emit AddLiquidity(msg.sender, amountA, amountB, liquidity);
     }
 
     function removeLiquidity(uint256 liquidity) external returns (uint256 amountA, uint256 amountB) {
-        require(liquidity > 0,"SimpleSwap: INSUFFICIENT_LIQUIDITY_BURNED");
-        address token0 = _token0; // gas savings
-        address token1 = _token1; // gas savings
+        require(liquidity > 0, "SimpleSwap: INSUFFICIENT_LIQUIDITY_BURNED");
 
-        (uint256 _reserve0, uint256 _reserve1) = getReserves(); // gas savings
+
+        (uint256 _reserve0, uint256 _reserve1) = getReserves();
         require(_reserve0 > 0 && _reserve1 > 0, "SimpleSwap: INSUFFICIENT_RESERVE_AMOUNT");
+        (address token0, address token1) = getTokens();
 
-        // 確認 pair 有回收到 lp token
-        _safeTransferFrom(address(this),liquidity);
-        uint256 balance = balanceOf(address(this));
-        require(balance > 0, "SimpleSwap: INSUFFICIENT_LIQUIDITY_BURNED");
+        // 執行 removeLiquidity 的 address 需要先 approve LP tokens 給 Pair contract (Router contract || 進行 ERC20-permit)
+        _safeTransferFrom(address(this), liquidity);
+
+        // 確認 Pair contract 有回收到 LP token
+        uint256 balance = balanceOf(address(this)); // openzeppelin ERC20 是採用 getter 讀取值
+        require(balance > liquidity, "SimpleSwap: INSUFFICIENT_LIQUIDITY_BURNED");
 
         // 利用 balance (liquidity) 和 totalSupply 來算要換回多少 tokens
         uint256 _totalSupply = totalSupply();
-        amountA = balance.mul(_reserve0) / _totalSupply; // using balances ensures pro-rata distribution
-        amountB = balance.mul(_reserve1) / _totalSupply; // using balances ensures pro-rata distribution
+        amountA = balance.mul(_reserve0) / _totalSupply;
+        amountB = balance.mul(_reserve1) / _totalSupply;
 
+        // 把回收的 LP token 燒掉，維持正確的 totalSupply
         require(amountA < _reserve0 && amountB < _reserve1, "SimpleSwap: INSUFFICIENT_LIQUIDITY_BURNED");
         _burn(address(this), balance);
 
+
+        // 歸回 tokens 給「burn LP token」的 address
         _safeTransfer(token0, msg.sender, amountA);
         _safeTransfer(token1, msg.sender, amountB);
 
+        // 更新 reserve 和 k
         uint256 balance0 = IERC20(token0).balanceOf(address(this));
         uint256 balance1 = IERC20(token1).balanceOf(address(this));
-
         _update(balance0, balance1);
-        kLast = balance0 * balance1;
+        kLast = balance0.mul(balance1);
 
         emit RemoveLiquidity(msg.sender, amountA, amountB, balance);
     }
@@ -125,7 +139,7 @@ contract SimpleSwap is ISimpleSwap, ERC20("Uniswap V2", "UNI-V2") {
         address token1 = _token1;
         require(tokenIn == token0 || tokenIn == token1, "SimpleSwap: INVALID_TOKEN_IN");
         require(tokenOut == token0 || tokenOut == token1, "SimpleSwap: INVALID_TOKEN_OUT");
-        require(tokenIn != tokenOut,"SimpleSwap: IDENTICAL_ADDRESS");
+        require(tokenIn != tokenOut, "SimpleSwap: IDENTICAL_ADDRESS");
 
         (uint256 reserve0, uint256 reserve1) = getReserves();
         /*
@@ -134,34 +148,42 @@ contract SimpleSwap is ISimpleSwap, ERC20("Uniswap V2", "UNI-V2") {
         */
         require(reserve0 > 0 && reserve1 > 0, "SimpleSwap: INSUFFICIENT_INPUT_AMOUNT");
         uint256 kForCeil = reserve0 * reserve1 - 1;
-        uint256 ceilForAmountOutA = (kForCeil - 1) / (reserve1 + amountIn) + 1;
-        uint256 ceilForAmountOutB = (kForCeil - 1) / (reserve0 + amountIn) + 1;
+        uint256 ceilForAmountOutA = kForCeil / (reserve1 + amountIn) + 1;
+        uint256 ceilForAmountOutB = kForCeil / (reserve0 + amountIn) + 1;
         require(reserve1 > ceilForAmountOutB, "SimpleSwap: INSUFFICIENT_INPUT_AMOUNT");
         require(reserve0 > ceilForAmountOutA, "SimpleSwap: INSUFFICIENT_INPUT_AMOUNT");
+
         // 算出 amountOut 是否有足夠的餘額能被提出
         amountOut = tokenIn == token0 ? reserve1 - ceilForAmountOutB : reserve0 - ceilForAmountOutA;
         /*
             把 tokenIn 放入池子
                 > 這邊一樣放錯轉錢的位置
-        */ 
+        */
         _safeTransferFrom(tokenIn, amountIn);
         _safeTransfer(tokenOut, msg.sender, amountOut);
 
-        uint256 balance0 = IERC20(token0).balanceOf(address(this)); 
+        // 確認最後換回的 reserve 還能維持 k 值
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
         uint256 balance1 = IERC20(token1).balanceOf(address(this));
-
         require(balance0 * balance1 >= kLast, "SimpleSwap: K");
-        _update(balance0,balance1);
-        kLast = balance0 * balance1;
+
+        // 更新出最後的 k
+        _update(balance0, balance1);
+        kLast = balance0.mul(balance1);
         emit Swap(msg.sender, tokenIn, tokenOut, amountIn, amountOut);
-        
     }
 
+    // --- GETTER: gas savings --- //
     function getTokenA() external view returns (address tokenA) {
         tokenA = _token0;
     }
 
     function getTokenB() external view returns (address tokenB) {
+        tokenB = _token1;
+    }   
+
+    function getTokens() public view returns (address tokenA, address tokenB) {
+        tokenA = _token0;
         tokenB = _token1;
     }
 
@@ -170,28 +192,41 @@ contract SimpleSwap is ISimpleSwap, ERC20("Uniswap V2", "UNI-V2") {
         reserveB = _reserveB;
     }
 
-    /*
-        一定要使用 call 來讓 msg.sender 變成合約，不然無法以合約身分調用 ERC20
-    */
+    function _getTokenBalance(address token) private view returns (uint256 balance) {
+        balance = IERC20(token).balanceOf(address(this));
+    }
+
+    // --- TransferHelper --- //
     function _safeTransfer(address token, address to, uint256 value) private {
+        // 一定要使用 call 來讓 msg.sender 變成合約，不然無法以合約身分調用 ERC20
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(_SELECTOR, to, value));
         require(success && (data.length == 0 || abi.decode(data, (bool))), "UniswapV2: TRANSFER_FAILED");
     }
 
-    /*
-        自己寫 TransferHelper
-    */
     function _safeTransferFrom(address token, uint256 amountAIn) private {
         bool success = IERC20(token).transferFrom(msg.sender, address(this), amountAIn);
         require(success, "UniswapV2: TRANSFER_FROM_FAILED");
     }
 
-    function _getTokenBalance(address token) private view returns (uint256 balance) {
-        balance = IERC20(token).balanceOf(address(this));
-    }
-
+    // --- Utils --- //
     function _update(uint256 balance0, uint256 balance1) private {
         _reserveA = balance0;
         _reserveB = balance1;
+    }
+
+        // 因為沒有拆分 core & periphery，所以把 reserve 也變成 params，降低 gas usage
+    function _quote(
+        uint256 amountAIn,
+        uint256 amountBIn,
+        uint256 _reserve0,
+        uint256 _reserve1
+    ) private pure returns (uint256 amountAOptimal, uint256 amountBOptimal) {
+        /*
+            > 這邊忘記要先確定 _reserve 是否為 0
+                _reserve == 0 : 初始增加流動性
+                _reserve != 0 : 已經有 k 值，要按照 k 去決定 amountA 和 amountB 之間的比例
+        */
+        amountAOptimal = _reserve0 == 0 ? amountAIn : (amountBIn * _reserve0) / _reserve1;
+        amountBOptimal = _reserve1 == 0 ? amountBIn : (amountAIn * _reserve1) / _reserve0;
     }
 }
